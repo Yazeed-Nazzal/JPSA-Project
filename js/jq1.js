3fd3db6d6cda6a48fd1b5452111ce061d4b857c8
yazeed nazal <yazeednazal96@gmail.com>
2019-06-27 17:14:41 +0300

    initial commit

---
+++ js/jq1.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+    // start nav bar
+    // start slide the nav br in mobile 
+    $(".menu-toogle").on("click",function() {
+    $("nav").slideToggle();
+    });
+    // end slide the nav br in mobile 
+    //end nav bar
+    //start 
+    var windowheight = screen.height,
+    headerheight = $("header").innerHeight();
+    if ($(window).innerWidth()<=450)
+    {
+        if (screen.height>=820)
+        {
+        $(".slider, .carousel-item ").height(windowheight  - windowheight/2 - 50);
+        $(".slider ").innerWidth($(window).innerWidth()).css("margin","10 0");
+        
+        }
+        else{
+        $(".slider, .carousel-item ").height(windowheight  - windowheight/2);
+        $(".slider ").innerWidth($(window).innerWidth()).css("margin","10 0");
+        }
+    }
+    else if($(window).innerWidth()>700 && $(window).innerWidth()<1030)
+    {
+
+        $(".slider, .carousel-item ").height(screen.height - 500);
+        $(".slider ").innerWidth($(window).innerWidth()).css("margin","10 0");
+        $(".content").css("top",-130)
+        $(".event-slider").css("top", -windowheight/8);
+
+    }
+    else
+    {
+    $(".slider, .carousel-item ").height(screen.height - 300);
+    $(".content").css("top",-70)
+    $(".event-slider").css("top", -windowheight/8);
+    }
+
+    $(window).on("resize",function () {
+
+        var windowheight = screen.height,
+        headerheight = $("header").innerHeight();
+        if ($(window).innerWidth()<=450)
+        {
+            if (screen.height>=820)
+            {
+            $(".slider, .carousel-item ").height(windowheight  - windowheight/2 - 50);
+            $(".slider ").innerWidth($(window).innerWidth()).css("margin","10 0");
+            
+            }
+            else{
+            $(".slider, .carousel-item ").height(windowheight  - windowheight/2);
+            $(".slider ").innerWidth($(window).innerWidth()).css("margin","10 0");
+            }
+        }
+        else if($(window).innerWidth()>700 && $(window).innerWidth()<1030)
+        {
+
+            $(".slider, .carousel-item ").height(screen.height - 500);
+            $(".slider ").innerWidth($(window).innerWidth()).css("margin","10 0");
+            $(".content").css("top",-130)
+
+        }
+        else
+        {
+        $(".slider, .carousel-item ").height(screen.height - 300);
+        $(".content").css("top",-70);
+        $(".event-slider").css("top", -90);
+        }
+    });
+   
+        
+    });
